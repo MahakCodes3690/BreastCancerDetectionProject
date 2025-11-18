b5a6a45eef85945b63604ec6ef2c62218b359a38
Mahak <mahakmittal257@gmail.com>
2025-11-18 14:37:16 +0530

    Initial commit - Flask app + model

--- dynamic_var.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app=Flask(__name__)
-
-@app.route('/index/<name>')
-def home(name):
-   return "my name is "+name
-
-if __name__=="__main__":
-    app.run(debug=True)
--- dynamicloop.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route('/shop/<user>')
-def home(user):
-    fruit_list=['mango','pineapple','strawbeery','guava']
-    return render_template('inherit.html',fruits=fruit_list,user=user)
-
-if __name__ == "__main__":
-    app.run(debug=True)
